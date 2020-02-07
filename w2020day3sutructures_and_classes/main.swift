c3c08b5bc47ed4fb3775c2707c8a5a2b432e61b8
MANBEERGIL <manbeergill545@gmail.com>
2020-02-07 17:59:53 -0500

    Initial Commit

---
+++ w2020day3sutructures_and_classes/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  w2020day3sutructures_and_classes
+//
+//  Created by MANBEER KAUR on 2020-02-07.
+//  Copyright © 2020 MANBEER KAUR. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
